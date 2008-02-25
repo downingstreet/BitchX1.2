63b0c17927569d143cdac6b5f45cf2f664cef09c
keaston <keaston@13b04d17-f746-0410-82c6-800466cd88b0>
2008-02-25 09:55:15 +0000

    Seed trunk from ircii-pana-1.1 branch.
    git-svn-id: http://bitchx.svn.sourceforge.net/svnroot/bitchx/trunk@4 13b04d17-f746-0410-82c6-800466cd88b0

---
+++ include/help.h
@@ -0,0 +1,16 @@
+/*
+ * help.h: header for help.c
+ *
+ * copyright(c) 1994 matthew green
+ *
+ * See the copyright file, or do a help ircii copyright 
+ *
+ * @(#)$Id$
+ */
+
+#ifndef __help_h
+# define __help_h
+
+	void	help (char *, char *, char *, char *);
+
+#endif /* __help_h */
